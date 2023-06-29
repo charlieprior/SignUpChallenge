44bf5092bbad9c6be6f0705bab0157c7761ae350
Charlie Prior <charlie@charlieprior.com>
2023-06-29 12:47:43 -0400

    Email validation working
    Small media query change

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-function subscribeClicked() {
-    const signup = document.querySelector('.signup');
-    const success = document.querySelector('.success');
-    signup.classList.add("hidden");
-    success.classList.remove("hidden");
+function validateEmail(email) {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
+
+function subscribeClicked(email) {
+    if (!validateEmail(email)) {
+        document.querySelector('.email__error').classList.add("error");
+        document.querySelector('.input').classList.add("error");
+    } else {
+        document.querySelector('.signup').classList.add("hidden");
+        document.querySelector('.success').classList.remove("hidden");
+        document.getElementById("success__email").innerHTML = email;
+    }
 }
 
 function dismissClicked() {
-    const signup = document.querySelector('.signup');
-    const success = document.querySelector('.success');
-    const form = document.querySelector('.signup__form');
-    signup.classList.remove("hidden");
-    success.classList.add("hidden");
-    form.reset();
+    document.querySelector('.signup').classList.remove("hidden");
+    document.querySelector('.success').classList.add("hidden");
+    document.querySelector('.email__error').classList.remove("error");
+    document.querySelector('.input').classList.remove("error");
+    document.querySelector('.signup__form').reset();
 }
